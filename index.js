2c633c9be001950de4bcf6ad7b0d3e2781d65451
hafsa696 <hafwirakullow@gmail.com>
2025-01-03 09:06:51 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+
+  const names = ["Charlie", "Samie", "Ali"];
+const event = "birthday";
+console.log(writeCards(names, event));
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
